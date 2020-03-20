686d728d1c37899eb9daa89835beb763a87817cf
lin-sel <ccnsy86@gmail.com>
2020-03-20 16:04:05 +0530

    Client Application Structure Created.

---
+++ Client/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Client/src/app/service/constant.spec.ts
@@ -0,0 +1,7 @@
+import { Constant } from './constant';
+
+describe('Constant', () => {
+  it('should create an instance', () => {
+    expect(new Constant()).toBeTruthy();
+  });
+});
