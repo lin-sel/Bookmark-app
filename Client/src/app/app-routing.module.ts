7bc7a02b9cd57e18a0199dd2fcc4bfeb261fbf27
lin-sel <ccnsy86@gmail.com>
2020-03-21 21:56:35 +0530

    Routing Added

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+
+const appRoutes: Routes = [
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+      // {
+      //       path: 'heroes',
+      //       component: HeroListComponent,
+      //       data: { title: 'Heroes List' }
+      // },
+      {
+            path: '',
+            redirectTo: '/login',
+            pathMatch: 'full'
+      },
+      { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+      imports: [RouterModule.forRoot(appRoutes)],
+      exports: [RouterModule]
 })
 export class AppRoutingModule { }
