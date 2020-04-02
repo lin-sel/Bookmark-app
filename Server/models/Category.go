3a55774734088c28d6cbc1351ab51c1adf7cc426
lin-sel <ccnsy86@gmail.com>
2020-04-02 06:09:37 +0530

    name change from BookmarkCategory to Category

---
+++ Server/models/Category.go
@@ -0,0 +1,35 @@
+package models
+
+import (
+	uuid "github.com/satori/go.uuid"
+)
+
+// Category Structure
+type Category struct {
+	Basemodel
+	CName    string     `gorm:"type:varchar(100);unique:name_userid" json:"category"`
+	UserID   uuid.UUID  `gorm:"type:varchar(40);not_null;unique:name_userid" json:"-"`
+	Bookmark []Bookmark `json:"bookmarks"`
+}
+
+// NewCategoryWithUserID Return Category Object
+func NewCategoryWithUserID(userid uuid.UUID) *Category {
+	return &Category{
+		UserID: userid,
+	}
+}
+
+//GetCategoryName return name
+func (category *Category) GetCategoryName() string {
+	return category.CName
+}
+
+//GetCategoryID return ID of Category
+func (category *Category) GetCategoryID() uuid.UUID {
+	return category.ID
+}
+
+//GetUserID return ID of User to which category Belong
+func (category *Category) GetUserID() uuid.UUID {
+	return category.UserID
+}
