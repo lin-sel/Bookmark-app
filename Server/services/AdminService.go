391d38f035d1505e138ac7a39d13a791e928dae6
lin-sel <ccnsy86@gmail.com>
2020-04-12 01:51:01 +0530

    GetAdmin() Added, Struct Change for Migration

--- Server/services/AdminService.go
+++ Server/services/AdminService.go
@@ -5,6 +5,7 @@ import (
 	"github.com/lin-sel/bookmark-app/models"
 	"github.com/lin-sel/bookmark-app/repository"
 	"github.com/lin-sel/bookmark-app/web"
+	uuid "github.com/satori/go.uuid"
 )
 
 // AdminService Struct Get Control On All Function
@@ -15,7 +16,7 @@ type AdminService struct {
 
 // NewAdminService Return New Instance Of AdminService
 func NewAdminService(db *gorm.DB, repo *repository.Repositorysrv) *AdminService {
-	db.AutoMigrate(models.Admin{})
+	db.AutoMigrate(models.User{})
 	return &AdminService{
 		DB:         db,
 		Repository: repo,
@@ -23,9 +24,9 @@ func NewAdminService(db *gorm.DB, repo *repository.Repositorysrv) *AdminService 
 }
 
 //Login admin Login
-func (service *AdminService) Login(requestedadmin *models.Admin) error {
+func (service *AdminService) Login(requestedadmin *models.User) error {
 	ufw := repository.NewUnitOfWork(service.DB, true)
-	var authadmin models.Admin
+	var authadmin models.User
 	err := service.Repository.GetByField(ufw, requestedadmin.Getusername(), "username", &authadmin, []string{})
 	if err != nil {
 		return web.NewValidationError("username", map[string]string{"error": "Invalid username"})
@@ -34,6 +35,19 @@ func (service *AdminService) Login(requestedadmin *models.Admin) error {
 		return web.NewValidationError("password", map[string]string{"error": "Invalid password"})
 
 	}
+	*requestedadmin = authadmin
+	return nil
+}
+
+//GetAdmin Return By ID
+func (service *AdminService) GetAdmin(requestedadmin *models.User) error {
+	ufw := repository.NewUnitOfWork(service.DB, true)
+	var authadmin models.User
+	err := service.Repository.GetByField(ufw, requestedadmin.GetID(), "id", &authadmin, []string{})
+	if (err != nil || authadmin.GetID() == uuid.UUID{}) {
+		return web.NewValidationError("user", map[string]string{"error": "Invalid User"})
+	}
+
 	*requestedadmin = authadmin
 	return nil
 }
