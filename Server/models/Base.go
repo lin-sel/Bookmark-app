d0f8b7c0351af7c6343bb67538d15443f2ab5de5
lin-sel <ccnsy86@gmail.com>
2020-04-12 01:41:06 +0530

    Unique Index Added in DeletedAt Field

--- Server/models/Base.go
+++ Server/models/Base.go
@@ -11,5 +11,5 @@ type Basemodel struct {
 	ID        uuid.UUID  `gorm:"type:varchar(36);primary_key;" json:"id"`
 	CreatedAt time.Time  `gorm:"column:createdOn" json:"-"`
 	UpdatedAt time.Time  `gorm:"column:updatedOn" json:"-"`
-	DeletedAt *time.Time `sql:"index" gorm:"column:deletedOn" json:"-"`
+	DeletedAt *time.Time `sql:"index" gorm:"column:deletedOn;unique_index:name_userid" json:"-"`
 }
