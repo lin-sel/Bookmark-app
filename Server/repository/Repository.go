e27b3b5ae5f1ac6c2be14bdd25d8acd23f9f4247
lin-sel <ccnsy86@gmail.com>
2020-04-12 01:48:30 +0530

    New Function GetTotalCount() Introduce and Refactor Code For BookmarkResponse and CategoryResponse struct

--- Server/repository/Repository.go
+++ Server/repository/Repository.go
@@ -16,6 +16,7 @@ type Repository interface {
 	Delete(ufw *UnitOfWork, id uuid.UUID, out interface{}) error
 	Update(ufw *UnitOfWork, id uuid.UUID, out interface{}) error
 	GetByField(ufw *UnitOfWork, value interface{}, fieldname string, preloadAssociation []string) error
+	GetTotalCount(ufw *UnitOfWork, id uuid.UUID, out interface{}) error
 }
 
 // Repositorysrv Return new Service
@@ -49,7 +50,7 @@ func (repo *Repositorysrv) Get(ufw *UnitOfWork, out interface{}, id uuid.UUID, p
 		category := out.(*[]models.Category)
 		return db.Model(out).Debug().First(out, "id = ? and user_id = ?", id, (*category)[0].UserID).Error
 	}
-	return errors.New("Unknown Error Occur")
+	return errors.New("Bad Request")
 }
 
 // GetAll Entity
@@ -61,15 +62,15 @@ func (repo *Repositorysrv) GetAll(ufw *UnitOfWork, id uuid.UUID, out interface{}
 	switch out.(type) {
 	case *[]models.User:
 		return db.Model(out).Debug().Find(out).Error
-	case *[]models.Bookmark:
-		bookmark := out.(*[]models.Bookmark)
-		fmt.Println((*bookmark)[0].UserID)
-		return db.Model(out).Debug().Find(out, "user_id = ?", (*bookmark)[0].UserID).Error
-	case *[]models.Category:
-		category := out.(*[]models.Category)
-		return db.Model(out).Debug().Find(out, "user_id = ?", (*category)[0].UserID).Error
+	case *models.BookmarkResponse:
+		response := out.(*models.BookmarkResponse)
+		return db.Debug().Model(response.ListOfBookmarkDTO).Limit(response.PageSize).Offset((response.PageNumber*response.PageSize)-response.PageSize).Find(response.ListOfBookmarkDTO, "user_id = ?", (*response.ListOfBookmarkDTO)[0].UserID).Error
+	case *models.CategoryResponse:
+		response := out.(*models.CategoryResponse)
+		return db.Debug().Model(response.ListOfCategoryDTO).Limit(response.PageSize).Offset((response.PageNumber*response.PageSize)-response.PageSize).Find(response.ListOfCategoryDTO, "user_id = ?", (*response.ListOfCategoryDTO)[0].UserID).Error
+		// return db.Model(out).Debug().Limit(2).Offset(0).Find(out, "user_id = ?", (*category)[0].UserID).Error
 	}
-	return errors.New("Unknown Error Occur")
+	return errors.New("Bad Request")
 }
 
 // Update Entity
@@ -80,16 +81,23 @@ func (repo *Repositorysrv) Update(ufw *UnitOfWork, entity interface{}) error {
 // Delete Entity From Database
 func (repo *Repositorysrv) Delete(ufw *UnitOfWork, id uuid.UUID, out interface{}) error {
 	switch out.(type) {
-	case *models.User:
-		return ufw.DB.Debug().Model(out).Delete(out, "id = ?", id).Error
 	case *models.Bookmark:
 		bookmark := out.(*models.Bookmark)
 		return ufw.DB.Debug().Model(out).Delete(out, "id = ? and user_id = ?", id, (*bookmark).UserID).Error
+	case *models.User:
+		return ufw.DB.Debug().Model(out).Delete(out, "id = ?", id).Error
 	case *models.Category:
 		category := out.(*models.Category)
+		bookmark := models.Bookmark{}
+		ufw.DB.Model(bookmark).Delete(bookmark, "category_id = ? and user_id = ?", id, (*category).UserID)
 		return ufw.DB.Debug().Model(out).Delete(out, "id = ? and user_id = ?", id, (*category).UserID).Error
 	}
-	return errors.New("Unknown error Occur")
+	return errors.New("Bad Request")
+}
+
+// DeleteByField Delete Based On Column Condition
+func (repo *Repositorysrv) DeleteByField(ufw *UnitOfWork, value interface{}, fieldname string, id uuid.UUID, out interface{}) {
+	ufw.DB.Model(out).Delete(out, fmt.Sprintf("%s = ? and user_id = ?", fieldname), id)
 }
 
 // GetByField Return Result Based on Field.
@@ -99,14 +107,29 @@ func (repo *Repositorysrv) GetByField(ufw *UnitOfWork, value interface{}, fieldn
 		db = db.Preload(association)
 	}
 	switch out.(type) {
-	case *models.User, *models.Admin:
+	case *models.User:
 		return db.Model(out).Debug().First(out, fmt.Sprintf("%s = ?", fieldname), value).Error
-	case *[]models.Bookmark:
-		bookmark := out.(*[]models.Bookmark)
-		return db.Model(out).Debug().Find(out, fmt.Sprintf("%s = ? and user_id = ?", fieldname), value, (*bookmark)[0].UserID).Error
+	case *models.BookmarkResponse:
+		response := out.(*models.BookmarkResponse)
+		return db.Debug().Model(response.ListOfBookmarkDTO).Limit(response.PageSize).Offset((response.PageNumber*response.PageSize)-response.PageSize).Find(response.ListOfBookmarkDTO, fmt.Sprintf("%s = ? and user_id = ?", fieldname), value, (*response.ListOfBookmarkDTO)[0].UserID).Error
 	case *[]models.Category:
 		category := out.(*[]models.Category)
 		return db.Model(out).Debug().First(out, fmt.Sprintf("%s = ? and user_id = ?", fieldname), value, (*category)[0].UserID).Error
 	}
-	return errors.New("Unknown error Occur")
+	return errors.New("Bad Request")
+}
+
+// GetTotalCount return total dataset.
+func (repo *Repositorysrv) GetTotalCount(ufw *UnitOfWork, id uuid.UUID, out interface{}, count interface{}, fieldname string, fieldvalue interface{}) error {
+	switch out.(type) {
+	case *models.Bookmark:
+		bookmark := out.(*models.Bookmark)
+		return ufw.DB.Debug().Model(out).Where(fmt.Sprintf("%s = ? and user_id = ?", fieldname), fieldvalue, bookmark.GetUserID()).Count(count).Error
+	case *models.Category:
+		category := out.(*models.Category)
+		return ufw.DB.Debug().Model(out).Where("user_id = ?", category.GetUserID()).Count(count).Error
+	case *models.User:
+		return ufw.DB.Debug().Model(out).Count(count).Error
+	}
+	return nil
 }
