f4dafa52058c45509313e8da54443d02d8ff570e
lin-sel <ccnsy86@gmail.com>
2020-04-12 01:57:42 +0530

    CheckUser() Function Added and Some Code Cleaning

--- Server/services/UserService.go
+++ Server/services/UserService.go
@@ -2,10 +2,12 @@ package services
 
 import (
 	"errors"
+	"strings"
 
 	"github.com/jinzhu/gorm"
 	"github.com/lin-sel/bookmark-app/models"
 	"github.com/lin-sel/bookmark-app/repository"
+	"github.com/lin-sel/bookmark-app/web"
 	uuid "github.com/satori/go.uuid"
 )
 
@@ -27,14 +29,18 @@ func NewUserService(db *gorm.DB, repo *repository.Repositorysrv) *UserService {
 // Register User
 func (auth *UserService) Register(user *models.User) error {
 	uow := repository.NewUnitOfWork(auth.DB, false)
-	authuser := models.User{}
-	err := auth.Repository.GetByField(uow, user.Getusername(), "username", &authuser, []string{})
-	if !authuser.IsEmpty() {
-		return errors.New("user Already present")
-	}
-	err = auth.Repository.Add(uow, user)
+	// authuser := models.User{}
+	// err := auth.Repository.GetByField(uow, user.Getusername(), "username", &authuser, []string{})
+	// fmt.Println(authuser)
+	// if !authuser.IsEmpty() {
+	// 	return errors.New("user Already present")
+	// }
+	err := auth.Repository.Add(uow, user)
 	if err != nil {
 		uow.Complete()
+		if strings.Contains(err.Error(), "Duplicate entry") {
+			return web.NewValidationError("user", map[string]string{"error": "user already present"})
+		}
 		return err
 	}
 	uow.Commit()
@@ -46,7 +52,7 @@ func (auth *UserService) Login(user *models.User) error {
 	uow := repository.NewUnitOfWork(auth.DB, true)
 	authuser := models.User{}
 	err := auth.Repository.GetByField(uow, user.Getusername(), "username", &authuser, []string{"Category"})
-	if authuser.IsEmpty() {
+	if err := authuser.IsUserValid(); err != nil {
 		return errors.New("Invalid Username")
 	}
 	if err != nil {
@@ -109,6 +115,7 @@ func (auth *UserService) Update(user *models.User) error {
 // Delete Return Auth User.
 func (auth *UserService) Delete(uid *uuid.UUID) error {
 	uow := repository.NewUnitOfWork(auth.DB, true)
+	// auth.Repository.Delete(uow, )
 	err := auth.Repository.Delete(uow, *uid, &models.User{})
 	if err != nil {
 		uow.Complete()
@@ -116,4 +123,28 @@ func (auth *UserService) Delete(uid *uuid.UUID) error {
 	}
 	uow.Commit()
 	return err
+}
+
+// CheckUser Parse UserID and check there model
+func (auth *UserService) CheckUser(param map[string]string, role string) (*uuid.UUID, error) {
+	userid, err := web.ParseID(param["userid"])
+	if err != nil {
+		return nil, web.NewValidationError("User ID", map[string]string{"error": "Invalid Admin ID"})
+	}
+	users := []models.User{*models.NewUserWithID(*userid)}
+	err = auth.Get(userid, &users)
+	if err != nil {
+		return nil, err
+	}
+	if len(users) <= 0 {
+		return nil, web.NewValidationError("User ID", map[string]string{"error": "Invalid Admin ID"})
+	}
+	if err := users[0].IsUserValid(); err != nil {
+		return nil, web.NewValidationError("User ID", map[string]string{"error": "Invalid Admin ID"})
+	}
+
+	if !users[0].IsEqualRole(role) {
+		return nil, web.NewValidationError("User ID", map[string]string{"error": "Invalid user"})
+	}
+	return userid, nil
 }
